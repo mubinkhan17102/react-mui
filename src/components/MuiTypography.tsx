6c93c7ff1fa2c9c46e65f218f28c3b92f04d03c7
mubinkhan17102 <mobin17102@gmail.com>
2023-02-01 15:41:17 +0600

    Typegraphy and button practice

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>H1 heading</Typography>
+        <Typography variant='h2' component={'h1'} gutterBottom>H2 heading</Typography>
+        <Typography variant='h3'>H3 heading</Typography>
+        <Typography variant='body2'>
+            This is body one. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cumque error perspiciatis, deleniti itaque vitae qui incidunt quo sit repudiandae nostrum, at, aliquid assumenda explicabo temporibus recusandae a ex exercitationem soluta veniam omnis distinctio praesentium. Aliquam voluptate est ab possimus autem ut esse soluta reiciendis dignissimos laborum quis officiis quasi, laboriosam at totam ea repudiandae pariatur aperiam. Deleniti repellat quasi aperiam nam. Animi aliquid officiis natus dolor quod unde, soluta veniam enim veritatis consequatur non error ex, nulla obcaecati totam molestias! Modi quasi molestias ipsam itaque aperiam ipsa, exercitationem ut quae esse dolores fuga laudantium doloremque, distinctio odit magni sequi cum quod dicta officia deserunt eligendi numquam. Eum, deleniti minus. At sequi in earum enim doloremque necessitatibus, magni quas? Ipsum animi tempore maiores molestias unde laborum possimus, asperiores rem necessitatibus assumenda eveniet, alias ea enim sequi deserunt quidem aspernatur? Quia ducimus blanditiis animi a, ullam rem reiciendis cumque explicabo, aliquid sint ipsa modi voluptatum eaque inventore harum nisi non perferendis incidunt nihil. Numquam eaque eligendi unde praesentium recusandae quae. Doloremque optio maxime alias libero cum! Expedita, aspernatur! Aperiam quisquam at autem dolores iusto officia dolorem delectus, veniam aspernatur deleniti natus eum, esse unde ab obcaecati eligendi quia mollitia culpa numquam.
+        </Typography>
+    </div>
+  )
+}
+
+export default MuiTypography
